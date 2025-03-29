d1fdd94d6cc19f062e55266e63c9a053d4b05998
sching76 <sching@gmx.de>
2025-03-29 08:26:41 +0100

    initial files from template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-pink-500 text-white text-3xl font-bold">
+      Hello world template with Tailwind CSS v3.4.3 🚀
+    </div>
+  );
+}
