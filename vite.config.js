c3506b2bf84d4f4a00868aa565921a6154f3712e
sching76 <sching@gmx.de>
2025-03-30 13:12:34 +0200

    removed warning

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split React and ReactDOM
+          react: ['react', 'react-dom', 'react-dom/client'],
+        },
+      },
+    },
+  },
 })
